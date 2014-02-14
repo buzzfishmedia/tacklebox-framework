c3caa7bf5637985bca6856030afd50f7204884a7
Frank McCoy <frank@mbp.fmccoy.info>
2014-02-14 00:01:40 -0600

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Index
+ * 
+ * @package tacklebox
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php if( have_posts() ) : ?>
+
+	<?php while( have_posts() ) : the_post(); ?>
+
+		<?php the_title(); ?>
+		<?php the_content(); ?>
+
+	<?php endwhile; ?>
+
+<?php endif; ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
