3dbe439e0c47224a30fdf04009e83a63c4fc0250
Frank McCoy <frankmccoy.d@gmail.com>
2014-03-02 22:40:16 -0600

    use wordpress helper classes

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
 
-/**
- * Include Composer Autoloader
- * 
- * @todo replace with composer autoloader
- */
-require_once( get_template_directory() . '/src/Theme.php' );
+if( defined('WP_COMPOSER') and WP_COMPOSER == false ){
+	require_once( get_template_directory() . '/src/Helpers/Theme.php' );
+	require_once( get_template_directory() . '/src/Helpers/AddMenuPage.php' );
+	require_once( get_template_directory() . '/src/Helpers/CustomPostType.php' );
+}
+
 
 // Initialize Theme Class
-$tacklebox = new Buzzfish\Theme('tacklebox');
+$tacklebox = new WordPress\Theme('tacklebox');
 
 // Theme Setup
 $setup = array(
@@ -19,9 +19,8 @@ $setup = array(
 		'post-formats' => array( 'aside', 'image', 'video', 'quote', 'link' )
 	),
 	'nav_menus' => array(
-		array( 'primary' => __('Primary Menu', 'tacklebox') ),
-		array( 'test-1' => __('Test 1', 'tacklebox') ),
-		array( 'test-2' => __('Test 2', 'tacklebox') )
+		array( 'primary'	=>	__('Primary Menu', 'tacklebox') ),
+		array( 'topbar'		=>	__('Top Bar', 'tacklebox') )
 	)
 );
 
@@ -58,28 +57,28 @@ if( ! is_admin() ){
 $widgets = array(
 	
 	array(
-		'name' => __('Footer One'),
+		'name' => __( 'Footer One', 'tacklebox' ),
 		'id' => 'footer-1',
 		'before_title' => '<h2 class="widget-title">',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => '</aside>'
 	),
 	array(
-		'name' => __('Footer Two'),
+		'name' => __( 'Footer Two', 'tacklebox' ),
 		'id' => 'footer-2',
 		'before_title' => '<h2 class="widget-title">',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => '</aside>'
 	),
 	array(
-		'name' => __('Footer Three'),
+		'name' => __( 'Footer Three', 'tacklebox' ),
 		'id' => 'footer-3',
 		'before_title' => '<h2 class="widget-title">',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget' => '</aside>'
 	),
 	array(
-		'name' => __('Sidebar'),
+		'name' => __( 'Sidebar', 'tacklebox' ),
 		'id' => 'sidebar-1',
 		'before_title' => '<h2 class="widget-title">',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
@@ -90,4 +89,36 @@ $widgets = array(
 $tacklebox->addWidgets($widgets);
 
 // Custom Header
-require_once( get_template_directory() . '/inc/custom-header.php' );
+//require_once( get_template_directory() . '/inc/custom-header.php' );
+
+require_once( get_template_directory().'/vendor/autoload.php' );
+
+
+/**
+ * Create Page for Composer
+ */
+
+$page = array( 'parent_slug' => 'tools.php' );
+$sections = array(
+	array('title' => 'Section 1'),
+	array('title' => 'Section 2')
+
+);
+$fields = array(
+	array(
+		'title' => 'Field 1',
+		'type' => 'text',
+		'section' => 'Section 1'
+	),
+	array(
+		'title' => 'Field 2',
+		'type' => 'text',
+		'section' => 'Section 2'
+	)
+);
+
+$composerpage = new WordPress\AddMenuPage("Get Composer", $page );
+$composerpage->setSections($sections);
+$composerpage->setFields($fields);
+
+
