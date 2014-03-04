e06069455b0a275368ec40da03d5b9526f898089
Frank McCoy <frankmccoy.d@gmail.com>
2014-03-04 11:00:35 -0600

    addded pagination

--- index.php
+++ index.php
@@ -9,38 +9,39 @@
 <?php get_header(); ?>
 
 <div class="container">
-
+	
 	<div id="content">
 
-		<div class="row">
-		
-			<div class="container">
+		<div class="container">
 
-				<div id="primary">
+			<div id="primary">
 
-					<div class="main" role="main">
+				<div class="main" role="main">
 
-					    <?php if( have_posts() ) : ?>
+					<?php if( have_posts() ) : ?>
 
-					    	<?php while( have_posts() ) : the_post(); ?>
+						<?php while( have_posts() ) : the_post(); ?>
 
-					    		<?php get_template_part( 'content', get_post_format() ); ?>
+							<?php get_template_part( 'content', get_post_format() ); ?>
 
-					    	<?php endwhile; ?>
+						<?php endwhile; ?>
 
-					    <?php endif; ?>
-					</div>
-					
+						<?php tacklebox_framework_paging_nav(); ?>
+
+					<?php endif; ?>
+
 				</div>
 
-				<?php get_sidebar(); ?>
-				
+			</div>
+
+			<?php get_sidebar(); ?>
+
 			</div>
 
 		</div>
-		
+
 	</div>
-	
+
 </div>
 
 <?php get_footer(); ?>
